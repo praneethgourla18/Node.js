cb0f8865adf12c90b7112c93af10dd10acf6c863
gourlapraneeth <35069891-gourlapraneeth@users.noreply.replit.com>
2024-09-07 05:40:20 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const {x,calculateSum}=require("./sum")
+// import {calculateSum , x} from "./sum.js";
+
+console.log("Hello World");
+var a=10;
+var b=20;
+calculateSum(10,20);
+console.log(x)
+
+// console.log(globalThis)
---
+++ sum.js
@@ -0,0 +1,10 @@
+console.log("sum module is executed")
+
+function calculateSum(a,b){
+  console.log(a+b)
+
+}
+console.log("hoii")
+ var x=100;
+
+module.exports ={x,calculateSum} ;
